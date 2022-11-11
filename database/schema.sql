a176168d8ed6fec9d45429e2c9d2412e6adb6a2a
lydiadingess <lydiadingess@gmail.com>
2022-11-10 20:42:51 -0500

    updates

---
+++ database/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE bigmacprice
+(
+	date date NOT NULL,
+	currency_code VARCHAR(5) NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	local_price numeric NOT NULL,
+	dollar_ex NUMERIC NOT NULL,
+	dollar_price numeric NOT NULL
+	
+);
+
+SELECT * FROM public.bigmacprice
+
+CREATE TABLE gdpmerge (	
+	Country VARCHAR(50) NOT NULL,
+	Code VARCHAR(5) NOT NULL,
+	year NUMERIC NOT NULL,
+	gdp_growth NUMERIC NOT NULL,
+	GDP_Per_Capita numeric NOT NULL,
+	GDP NUMERIC NOT NULL,
+	country_code NUMERIC NOT NULL,
+	region VARCHAR(50) NOT NULL,
+	sub_region VARCHAR(100) NOT NULL
+);
+
